5ebb9d34675247afa11aac56b753a75b2fd31cac
MICKEY-EXIDE <mickeyofficial448@gmail.com>
2024-12-03 17:31:45 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "94764609656"
+const SESSION_ID = "PRABATH-MD~470TlLJK#B3fCqhGY8z3GAanuTl9pCpyai1FcrLyEnmwOIAoGk_E"
 
 
 module.exports = {
